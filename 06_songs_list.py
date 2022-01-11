4d0a6bed830c2fdd4c867806790a4bb618a38a57
VIPER <blizzvenom@gmail.com>
2022-01-11 15:55:26 +0300

    Done 6 task

--- 06_songs_list.py
+++ 06_songs_list.py
@@ -20,7 +20,14 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+sum_leng_songs = 0
+sum_leng_songs += violator_songs_list[3][1]
+sum_leng_songs += violator_songs_list[5][1]
+sum_leng_songs += violator_songs_list[-1][1]
+sum_leng_songs = round(sum_leng_songs,3)
+
+print('Summary sound length of 3 songs', sum_leng_songs, 'min')
+
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -35,7 +42,22 @@ violator_songs_dict = {
     'Clean': 5.68,
 }
 
+
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+#Суммирование указанных трех песен в словаре
+dict_len = 0
+dict_len += violator_songs_dict.get('Sweetest Perfection')
+dict_len += violator_songs_dict.get('Policy of Truth')
+dict_len += violator_songs_dict.get('Blue Dress')
+dict_len = round(dict_len,3)
+
+#Суммирование оставшихся песен в словаре
+dict_len_last = 0
+dict_len_last += violator_songs_dict.get('World in My Eyes')
+dict_len_last += violator_songs_dict.get('Personal Jesus')
+dict_len_last += violator_songs_dict.get('Waiting for the Night')
+dict_len_last = round(dict_len_last,3)
+
+print('Summ len of 3 songs is ',dict_len,'min.', 'And last 3 is', dict_len_last, 'min.')
