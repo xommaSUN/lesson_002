e6716673213f86d45eb4445a34916edb3099f0e5
VIPER <blizzvenom@gmail.com>
2022-01-11 16:18:09 +0300

    Done 7 task

--- 07_secret.py
+++ 07_secret.py
@@ -21,4 +21,23 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+#print(len(secret_message[4]))
+
+message = ''
+
+message = secret_message[0][3]
+print(message)
+
+message = secret_message[1][9:13]
+print(message)
+
+message = secret_message[2][5:15:2]
+print(message)
+
+message = secret_message[3][12:6:-1]
+print(message)
+
+message = secret_message[4][20:15:-1]
+print(message)
+
+
